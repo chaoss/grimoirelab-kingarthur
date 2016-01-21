beff95cf9a6a3242693a7101cc94dc1caa3f490e
Santiago Dueñas <sduenas@bitergia.com>
2016-01-21 20:51:20 +0100

    [errors] Add base exceptions

---
+++ arthur/errors.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2016 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#     Alvaro del Castillo San Felix <acs@bitergia.com>
+#
+
+class BaseError(Exception):
+    """Base class for Arthur exceptions.
+
+    Derived classes can overwrite the error message declaring ``message``
+    property.
+    """
+    message = "Arthur base error"
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.msg = self.message % kwargs
+
+    def __str__(self):
+        return self.msg
+
+
+class NotFoundError(BaseError):
+    """Exception raised when an element is not found"""
+
+    message = "%(element)s not found"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.element = kwargs['element']
---
+++ tests/test_errors.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2016 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#
+
+import sys
+
+if not '..' in sys.path:
+    sys.path.insert(0, '..')
+
+import unittest
+
+import arthur.errors as errors
+
+
+# Mock classes to test BaseError class
+class MockErrorNoArgs(errors.BaseError):
+    message = "Mock error without args"
+
+
+class MockErrorArgs(errors.BaseError):
+    message = "Mock error with args. Error: %(code)s %(msg)s"
+
+
+class TestBaseError(unittest.TestCase):
+
+    def test_subblass_with_no_args(self):
+        """Check subclasses that do not require arguments.
+
+        Arguments passed to the constructor should be ignored.
+        """
+        e = MockErrorNoArgs(code=1, msg='Fatal error')
+
+        self.assertEqual(str(e), "Mock error without args")
+
+    def test_subclass_args(self):
+        """Check subclasses that require arguments"""
+
+        e = MockErrorArgs(code=1, msg='Fatal error')
+
+        self.assertEqual(str(e), "Mock error with args. Error: 1 Fatal error")
+
+    def test_subclass_invalid_args(self):
+        """Check when required arguments are not given.
+
+        When this happens, it raises a KeyError exception.
+        """
+        kwargs = {'code' : 1, 'error' : 'Fatal error'}
+        self.assertRaises(KeyError, MockErrorArgs, **kwargs)
+
+
+class TestNotFoundError(unittest.TestCase):
+
+    def test_message(self):
+        """Make sure that prints the correct error"""
+
+        e = errors.NotFoundError(element='repository')
+        self.assertEqual(str(e), 'repository not found')
+
+    def test_entity(self):
+        """Entity attribute should have a value"""
+
+        e = errors.NotFoundError(element='repository')
+        self.assertEqual(e.element, 'repository')
+
+
+if __name__ == "__main__":
+    unittest.main()

f1be3b17e475458e525af7ef9ba45191e8e1ac61
Santiago Dueñas <sduenas@bitergia.com>
2016-02-19 14:22:04 +0100

    [arthur] Add main class to manage the system

---
+++ arthur/arthur.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2016 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#     Alvaro del Castillo San Felix <acs@bitergia.com>
+#
+
+import logging
+import pickle
+
+from rq import push_connection
+
+from .common import Q_CREATION_JOBS, Q_STORAGE_ITEMS
+from .repositories import RepositoryManager
+from .scheduler import Scheduler
+
+
+logger = logging.getLogger(__name__)
+
+
+class Arthur:
+    """Main class to retrieve data from software repositories."""
+
+    def __init__(self, conn, async_mode=True):
+        self.conn = conn
+        self.conn.flushdb()
+        push_connection(self.conn)
+
+        self.repositories = RepositoryManager()
+        self.scheduler = Scheduler(async_mode=async_mode)
+
+    def add(self, origin, backend, args):
+        """Add and schedule a repository."""
+
+        self.repositories.add(origin, backend, **args)
+        repository = self.repositories.get(origin)
+        self.scheduler.add_job(Q_CREATION_JOBS, repository)
+
+    def items(self):
+        """Get the items fetched by the jobs."""
+
+        for item in self.conn.lrange(Q_STORAGE_ITEMS, 0, -1):
+            item = pickle.loads(item)
+            yield item
---
+++ tests/test_arthur.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2016 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#
+
+import sys
+import unittest
+
+if not '..' in sys.path:
+    sys.path.insert(0, '..')
+
+from arthur.arthur import Arthur
+
+from tests import find_empty_redis_database
+
+
+class TestArthur(unittest.TestCase):
+    """Unit tests for Scheduler class"""
+
+    def setUp(self):
+        self.conn = find_empty_redis_database()
+        self.conn.flushdb()
+
+    def tearDown(self):
+        self.conn.flushdb()
+
+    def test_items(self):
+        app = Arthur(self.conn, async_mode=False)
+        app.add('test', 'git', args={'gitlog' : 'data/git_log.txt'})
+        commits = [item['commit'] for item in app.items()]
+
+        expected = ['bc57a9209f096a130dcc5ba7089a8663f758a703',
+                    '87783129c3f00d2c81a3a8e585eb86a47e39891a',
+                    '7debcf8a2f57f86663809c58b5c07a398be7674c',
+                    'c0d66f92a95e31c77be08dc9d0f11a16715d1885',
+                    'c6ba8f7a1058db3e6b4bc6f1090e932b107605fb',
+                    '589bb080f059834829a2a5955bebfd7c2baa110a',
+                    'ce8e0b86a1e9877f42fe9453ede418519115f367',
+                    '51a3b654f252210572297f47597b31527c475fb8',
+                    '456a68ee1407a77f3e804a30dff245bb6c6b872f']
+
+        self.assertListEqual(commits, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()

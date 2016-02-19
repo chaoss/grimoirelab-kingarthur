74983bb5620f5d95e95b73672908f4e4b0df4c86
Santiago Dueñas <sduenas@bitergia.com>
2016-02-19 14:05:42 +0100

    [scheduler] Add class to schedule jobs

---
+++ arthur/scheduler.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2016 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#     Alvaro del Castillo San Felix <acs@bitergia.com>
+#
+
+import logging
+
+from rq import Queue
+
+from .common import Q_CREATION_JOBS, Q_UPDATING_JOBS, Q_STORAGE_ITEMS
+from .errors import NotFoundError
+from .jobs import execute_perceval_job
+
+
+logger = logging.getLogger(__name__)
+
+
+class Scheduler:
+    """Scheduler of jobs.
+
+    This class is able to schedule Perceval jobs. Jobs are added to
+    two predefined queues: one for creation of repositories and
+    one for updating those repositories.
+
+    :param asyc_mode: set to run in asynchronous mode
+    """
+    def __init__(self, async_mode=True):
+        self.queues = {
+                       Q_CREATION_JOBS : Queue(Q_CREATION_JOBS, async=async_mode),
+                       Q_UPDATING_JOBS : Queue(Q_UPDATING_JOBS, async=async_mode)
+                      }
+
+    def add_job(self, queue_id, repository):
+        """Add a Perceval job to a queue.
+
+        :param queue_id: identifier of the queue where the job will be added
+        :param repository: input repository for the job
+
+        :returns: the scheduled job
+
+        :raises NotFoundError: when the queue set to run the job does not
+            exist.
+        """
+        if queue_id not in self.queues:
+            raise NotFoundError(element=queue_id)
+
+        job = self.queues[queue_id].enqueue(execute_perceval_job,
+                                            qitems=Q_STORAGE_ITEMS,
+                                            origin=repository.origin,
+                                            backend=repository.backend,
+                                            **repository.kwargs)
+
+        logging.debug("Job #%s %s (%s) enqueued in '%s' queue",
+                      job.get_id(), repository.origin,
+                      repository.backend, queue_id)
+        return job
---
+++ tests/test_scheduler.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2016 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#
+
+import sys
+import unittest
+
+if not '..' in sys.path:
+    sys.path.insert(0, '..')
+
+from arthur.common import Q_CREATION_JOBS, Q_UPDATING_JOBS
+from arthur.errors import NotFoundError
+from arthur.repositories import Repository
+from arthur.scheduler import Scheduler
+
+from tests import TestBaseRQ
+
+
+class TestScheduler(TestBaseRQ):
+    """Unit tests for Scheduler class"""
+
+    def test_queues_initialization(self):
+        """Queues must be created"""
+
+        schlr = Scheduler(async_mode=False)
+
+        self.assertIn(Q_CREATION_JOBS, schlr.queues)
+        self.assertEqual(schlr.queues[Q_CREATION_JOBS]._async, False)
+
+        self.assertIn(Q_UPDATING_JOBS, schlr.queues)
+        self.assertEqual(schlr.queues[Q_UPDATING_JOBS]._async, False)
+
+    def test_add_job(self):
+        """Jobs should be added and executed"""
+
+        repo = Repository('test', 'git', gitlog='data/git_log.txt')
+
+        schlr = Scheduler(async_mode=False)
+        job = schlr.add_job(Q_CREATION_JOBS, repo)
+
+        self.assertEqual(job.return_value, 'Tue Feb 11 22:10:39 2014 -0800')
+
+    def test_not_found_queue(self):
+        """Raises an error when a queue does not exist"""
+
+        repo = Repository('test', 'git', gitlog='data/git_log.txt')
+        schlr = Scheduler(async_mode=False)
+
+        self.assertRaises(NotFoundError, schlr.add_job, 'myqueue', repo)
+
+
+if __name__ == "__main__":
+    unittest.main()

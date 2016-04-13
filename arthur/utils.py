0595e509c8eb5859464b1eff3e996799f6a13985
Santiago Dueñas <sduenas@bitergia.com>
2016-04-13 13:41:36 +0200

    [utils] Add read-write lock to solve readers-writers problem

---
+++ arthur/utils.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2016 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#     Alvaro del Castillo San Felix <acs@bitergia.com>
+#
+
+import threading
+
+
+class RWLock:
+    """Read Write lock to avoid starvation.
+
+    This lock solves the problem of the so-called 'third readers-writers
+    problem'. The implementation is based on the algorithm proposed by
+    Samuel Tardieu on his article 'The third readers-writers problem' from
+    his blog page. Please visit the next link for more information:
+
+    https://www.rfc1149.net/blog/2011/01/07/the-third-readers-writers-problem/
+    """
+    def __init__(self):
+        self._readers = 0
+        self._order_mutex = threading.Semaphore()
+        self._readers_mutex = threading.Semaphore()
+        self._access_mutex = threading.Semaphore()
+
+    def reader_acquire(self):
+        """Acquire the lock to read"""
+
+        self._order_mutex.acquire()
+        self._readers_mutex.acquire()
+
+        if self._readers == 0:
+            self._access_mutex.acquire()
+        self._readers += 1
+
+        self._order_mutex.release()
+        self._readers_mutex.release()
+
+    def reader_release(self):
+        """Release the lock after reading"""
+
+        self._readers_mutex.acquire()
+
+        self._readers -= 1
+        if self._readers == 0:
+            self._access_mutex.release()
+
+        self._readers_mutex.release()
+
+    def writer_acquire(self):
+        """Acquire the lock to write"""
+
+        self._order_mutex.acquire()
+        self._access_mutex.acquire()
+        self._order_mutex.release()
+
+    def writer_release(self):
+        """Release the lock after writting"""
+
+        self._access_mutex.release()
---
+++ tests/test_utils.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2016 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#
+
+import copy
+import sys
+import threading
+import time
+import unittest
+
+if not '..' in sys.path:
+    sys.path.insert(0, '..')
+
+from arthur.utils import RWLock
+
+
+class RWLockThread(threading.Thread):
+
+    def __init__(self, lock, rw_buffer, sleep_before, sleep_after):
+        super().__init__()
+        self.lock = lock
+        self.rw_buffer = rw_buffer
+        self.sleep_before = sleep_before
+        self.sleep_after = sleep_after
+        self.entry_time = None
+        self.exit_time = None
+
+
+class Reader(RWLockThread):
+
+    def __init__(self, lock, rw_buffer, sleep_before, sleep_after):
+        super().__init__(lock, rw_buffer, sleep_before, sleep_after)
+        self.rdata = None
+
+    def run(self):
+        time.sleep(self.sleep_before)
+        self.lock.reader_acquire()
+        self.entry_time = time.time()
+        self.rdata = copy.deepcopy(self.rw_buffer)
+        time.sleep(self.sleep_after)
+        self.exit_time = time.time()
+        self.lock.reader_release()
+
+
+class Writer(RWLockThread):
+
+    def __init__(self, lock, rw_buffer, sleep_before, sleep_after, data):
+        super().__init__(lock, rw_buffer, sleep_before, sleep_after)
+        self.wdata = data
+
+    def run(self):
+        time.sleep(self.sleep_before)
+        self.lock.writer_acquire()
+        self.entry_time = time.time()
+        self.rw_buffer[0] = self.wdata
+        time.sleep(self.sleep_after)
+        self.exit_time = time.time()
+        self.lock.writer_release()
+
+
+class TestRWLock(unittest.TestCase):
+    """Unit tests for RWLock class"""
+
+    def test_multiple_readers(self):
+        """Test non-exclusive access to readers"""
+
+        rw_lock = RWLock()
+        rw_buffer = ['A']
+
+        threads = [Reader(rw_lock, rw_buffer, 0, 0.05),
+                   Reader(rw_lock, rw_buffer, 0.01, 0),
+                   Writer(rw_lock, rw_buffer, 0.02, 0.02, 'Z'),
+                   Reader(rw_lock, rw_buffer, 0.03, 0)]
+
+        for th in threads:
+            th.start()
+        for th in threads:
+            th.join()
+
+        self.assertEqual(rw_buffer, ['Z'])
+        self.assertEqual(threads[0].rdata, ['A'])
+        self.assertEqual(threads[1].rdata, ['A'])
+        self.assertEqual(threads[3].rdata, ['Z'])
+
+        # Second reader started after the first one finishing
+        # before it. This means there is no mutual exclusion
+        # between readers.
+        self.assertLess(threads[0].entry_time, threads[1].entry_time)
+        self.assertGreater(threads[0].exit_time, threads[1].exit_time)
+
+    def test_exclusion_writers(self):
+        """Test if only one writer can access the resource at the same time"""
+
+        rw_lock = RWLock()
+        rw_buffer = ['A']
+
+        threads = [Reader(rw_lock, rw_buffer, 0, 0.02),
+                   Writer(rw_lock, rw_buffer, 0.02, 0.03, 'Z'),
+                   Writer(rw_lock, rw_buffer, 0.03, 0, 'X'),
+                   Reader(rw_lock, rw_buffer, 0.04, 0)]
+
+        for th in threads:
+            th.start()
+        for th in threads:
+            th.join()
+
+        self.assertEqual(rw_buffer, ['X'])
+        self.assertEqual(threads[0].rdata, ['A'])
+        self.assertEqual(threads[3].rdata, ['X'])
+
+        # First writting lasts more than the time that the second
+        # writer needs to access the critical section. But, the
+        # second writer will wait till the first writer ends its work.
+        self.assertLess(threads[1].entry_time, threads[2].entry_time)
+        self.assertLess(threads[1].exit_time, threads[2].exit_time)
+
+        # The last thread (a reader) will wait till the last writer ends.
+        self.assertLess(threads[2].entry_time, threads[3].entry_time)
+        self.assertLess(threads[2].exit_time, threads[3].exit_time)
+
+
+if __name__ == "__main__":
+    unittest.main()
